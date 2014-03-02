a900b14305d78f5ae8bcb582e9773cbf9e090996
toritori0318 <toritori0318@gmail.com>
2014-03-03 03:14:06 +0900

    remove unused method

--- lib/Toy/Box/Misawa.pm
+++ lib/Toy/Box/Misawa.pm
@@ -3,8 +3,6 @@ use strict;
 use warnings;
 use parent 'Toy::Box::Base';
 use XML::FeedPP;
-use Web::Scraper;
-use URI;
 
 sub new {
     my $class = shift;
@@ -34,17 +32,4 @@ sub get {
     return \@rows;
 }
 
-sub _scraper {
-    my ($self, $path) = @_;
-
-    my $url = sprintf("%s/%s", $self->{site_url}, $path);
-    my $uri = URI->new($url);
-    my $scraper = scraper {
-        process '//input[@id="imageUrl"]', 'image_url' => '@value';
-        process '//input[@id="dataUrl"]',  'data_url'  => '@value';
-    };
-    return $scraper->scrape($uri);
-}
-
 1;
-
